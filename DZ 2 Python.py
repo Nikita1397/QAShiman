2ae139911c9e3763d4fd91a7cf8dd92369ea4ad7
Nikita1397 <76530198+Nikita1397@users.noreply.github.com>
2021-03-01 11:41:54 +0300

    Add files via upload

---
+++ DZ 2 Python.py
@@ -0,0 +1,26 @@
+# Задача 1
+# Выведите на экран надпись"Nothing will work unless you do" на разных строках двумя способами используя функцию print
+
+#1 Вариант
+print("Nothing will work\nunless you do")
+
+#2 вариант
+print("Nothing will work")
+print()
+print("unless you do")
+
+
+# Задача 2
+# Выведите на экран надпись " Anyone who stops learning s old, whether at twenty or eighty" Henry Ford на разных строках.
+
+print( "Anyone who\n        stops\n        earning s old,\n whether at twenty or eighty \n   Henry Ford")
+
+
+
+
+
+
+
+
+
+
